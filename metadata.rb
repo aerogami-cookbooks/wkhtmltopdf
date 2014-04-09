e07a7f0a4db41b77bd3f482c2e61dffedb3104a1
Mohamad El-Husseini <husseini.mel@gmail.com>
2014-04-09 12:55:12 -0300

    fixed metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,9 @@ license          'Apache 2.0'
 description      'Installs wkhtmltopdf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe 'wkhtmltopdf', 'Installs wkhtmltopdf'
+
+supports 'ubuntu'
+
+depends 'apt'
